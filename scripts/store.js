ae6571181c478a4b12500b67ea9d5d187e5ef4a4
Mhatw <contactomhatw@gmail.com>
2022-06-04 13:33:28 -0500

    add STORE

---
+++ scripts/store.js
@@ -0,0 +1,12 @@
+let STORE = {
+  user: null,
+  // contacts: [],
+  // favorites: [],
+  // currentContact: null,
+  // fetchContacts,
+  // deleteContact,
+  // favoriteContact,
+  // unFavoriteContact,
+};
+
+export default STORE;
