f28712e1920090975be0e15f6bbe9e93a73be726
Mhatw <contactomhatw@gmail.com>
2022-06-04 13:46:38 -0500

    modularization aside

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { tokenKey } from "./scripts/config.js";
 import DOMHandler from "./scripts/dom-handler.js";
+import { HomePage } from "./scripts/renders/home.js";
 import LoginPage from "./scripts/renders/login.js";
 async function init() {
   try {
@@ -11,7 +12,7 @@ async function init() {
     DOMHandler.load(LoginPage);
   } catch (error) {
     sessionStorage.removeItem(tokenKey);
-    DOMHandler.load(LoginPage);
+    DOMHandler.load(HomePage);
   }
 }
 
--- scripts/renders/home.js
+++ scripts/renders/home.js
@@ -1,59 +1,11 @@
+import { renderAside } from "./aside.js";
+
 function renderHome() {
   return `
   <main class="mainApp">
       <!-- aside -->
-
-      <aside class="lateralBar">
-        <ul>
-          <div>
-            <!-- logo -->
-            <li class="asideLogo">
-              <span class="icon-text">
-                <span>{ organizable }</span>
-              </span>
-            </li>
-
-            <!-- my boards -->
-            <li class="asideMyBoards">
-              <span class="icon-text">
-                <span class="icon">
-                  <i class="fas fa-solid fa-border-all"></i>
-                </span>
-                <span>My Boards</span>
-              </span>
-            </li>
-
-            <!-- closed boards -->
-            <li class="asideClosedBoards">
-              <span class="icon-text">
-                <span class="icon">
-                  <i class="fas fa-solid fa-trash"></i>
-                </span>
-                <span>Closed Boards</span>
-              </span>
-            </li>
-
-            <!-- my profile -->
-            <li class="asideMyProfile">
-              <span class="icon-text">
-                <span class="icon">
-                  <i class="fas fa-solid fa-id-badge"></i>
-                </span>
-                <span>My Profile</span>
-              </span>
-            </li>
-          </div>
-          <!-- logout -->
-          <li class="asideLogout">
-            <span class="icon-text">
-              <span class="icon">
-                <i class="fas fa-solid fa-power-off"></i>
-              </span>
-              <span>Log out</span>
-            </span>
-          </li>
-        </ul>
-      </aside>
+      ${renderAside()}
+      
       <!-- right panel -->
       <section class="appBoards">
         <div class="myBoards">
